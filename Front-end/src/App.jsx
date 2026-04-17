b96091a269789670acdd55df9a6cc377da722db4
mamillo <brandonmamillo5@gmail.com>
2026-04-17 12:05:30 +0300

    Added the cors.php header file to be used to link other php files to the front-end

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import PasswordReset from "./components/password";
+import Login from "./components/Login";
 
 
 
 function App() {
   return (
     <>
-        <PasswordReset />
+        <Login />
     
     </>
   );
